5dc2c32cbf67404c0e755619053e540b888586b9
seunghyun0522 <cvtcvt007@naver.com>
2024-03-13 15:58:08 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import TravelList from "./components/TravelList";
+import styled from "styled-components";
 
-export default function App() {
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
   return (
-    <main>
-      <TravelList />
-    </main>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
+
+export default App;
